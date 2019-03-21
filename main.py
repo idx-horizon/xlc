91e4117d15731122ec62279fdc148dfd465cee17
idx <idx-horizon@users.noreply.github.com>
2019-03-21 23:15:02 +0000

    a

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-#from app.converters import DS 
+from app.converters import DSFile as DS
 #FN = 'testdata/TEST.xlsx'
 if __name__ == '__main__':
-  #  w = DS('testdata/TEST.xlsx')
-  #  
-  #  if w is not None: 
-  #      w.analyse()
-  #  else:
-  #      print('** Error: Unable to instantiate object')  
-  pass
+    w = DS('testdata/TEST.xlsx')
+    
+    if w is not None: 
+       w.analyse()
+    else:
+        print('** Error: Unable to instantiate object')  
+  
