5b1215bb11928b852107a0eec29c2493081ba0c4
idx <idx-horizon@users.noreply.github.com>
2019-03-21 21:55:45 +0000

    a

--- xlc.py
+++ xlc.py
@@ -3,7 +3,7 @@ from app.converters import DS
 if __name__ == '__main__':
     w = DS('testdata/TEST.xlsx')
     
-    if w is not None 
+    if w is not None: 
         w.analyse()
     else 
         print('** Error: Unable to instantiate object')  
