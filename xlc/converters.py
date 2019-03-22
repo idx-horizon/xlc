e5ee67efa76f22df67ca08f7cc7f012445f5ac62
idx <idx-horizon@users.noreply.github.com>
2019-03-22 13:14:27 +0000

    a

--- xlc/converters.py
+++ xlc/converters.py
@@ -6,9 +6,6 @@ class DSFile(object):
 
   def __init__(self, 
                source_file, 
-               dest_file=None, 
-               sheet=None, 
-               header=None, 
                options=None):
   
     if not os.path.exists(source_file):
@@ -17,23 +14,30 @@ class DSFile(object):
       
     self.source_file = source_file
     self.basename = os.path.basename(source_file)
-    self.dest_file = dest_file or source_file + '_converted.csv'
-    self.sheet = sheet or 0
-    self.header = header
     self.options = options
     
+  def open():
+    return xlrd.open_workbook(self.source_file)
+    
   def analyse(self,sheets=None):
-    wb = xlrd.open_workbook(self.source_file)
+    wb = self.open()
     print('Source: {}, #sheets'.format(self.basename, wb.nsheets))
      
     for sh in wb.sheets():
       print('Name: {} Rows: {} Cols: {}'.format(sh.name, sh.ncols, sh.nrows))
       
-  def convert(self):
-    with open(self.dest_file,'w') as fh_out:
-      dw = csv.DictWriter(fh_out, fieldnames=['A','B'], lineterminator='\n')
+  def convert(self, dest_file=None, sheet=0, header=0):
+    outfile = dest_file or self.source_file + '_converted.csv'
+    
+    wb = self.open()
+    sh = wb.sheet_by_index[sheet]
+    col_headers = sh.row(1)
+    
+    with open(out_file,'w') as fh_out:
+      dw = csv.DictWriter(fh_out, fieldnames=col_headers, lineterminator='\n')
       dw.writeheader()
-      dw.writerow({'A': 1, 'B': 'Not yet implemented'})
+      for r in sh.get_rows():
+        dw.writerow(r)
       
     print('Not yet implemented')
     
