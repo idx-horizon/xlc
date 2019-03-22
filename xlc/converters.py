e76a2cb3d542308444a10b04e5080868626434bf
idx <idx-horizon@users.noreply.github.com>
2019-03-22 13:18:21 +0000

    a

--- xlc/converters.py
+++ xlc/converters.py
@@ -21,16 +21,16 @@ class DSFile(object):
     
   def analyse(self,sheets=None):
     wb = self.open()
-    print('Source: {}, #sheets'.format(self.basename, wb.nsheets))
+    print('Source: {}, #sheets: {}'.format(self.basename, wb.nsheets))
      
     for sh in wb.sheets():
       print('Name: {} Rows: {} Cols: {}'.format(sh.name, sh.ncols, sh.nrows))
       
   def convert(self, dest_file=None, sheet=0, header=0):
-    outfile = dest_file or self.source_file + '_converted.csv'
+    outfile = dest_file or self.source_file +'_sheet_' + str(sheet) + '.csv'
     
     wb = self.open()
-    sh = wb.sheet_by_index[sheet]
+    sh = wb.sheet_by_index(sheet)
     col_headers = sh.row(1)
     
     with open(out_file,'w') as fh_out:
