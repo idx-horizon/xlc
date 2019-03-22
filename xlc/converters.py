f56a7114f26391176491d7c717fbda6cb7ee41cc
idx <idx-horizon@users.noreply.github.com>
2019-03-22 12:54:56 +0000

    a

--- xlc/converters.py
+++ xlc/converters.py
@@ -5,17 +5,18 @@ import os
 class DSFile(object):
 
   def __init__(self, 
-  source_file, 
-  dest_file=None, 
-  sheet=None, 
-  header=None, 
-  options=None):
+               source_file, 
+               dest_file=None, 
+               sheet=None, 
+               header=None, 
+               options=None):
   
     if not os.path.exists(source_file):
       print('** File does not exist {}'.format(source_file))
       return None
       
     self.source_file = source_file
+    self.basename = os.path.basename(source_file)
     self.dest_file = dest_file
     self.sheet = sheet
     self.header = header
@@ -23,9 +24,15 @@ class DSFile(object):
     
   def analyse(self,sheets=None):
     wb = xlrd.open_workbook(self.source_file)
+    print('Source: {}, #sheets'.format(self.basename, sh.nsheets), 
     for sh in wb.sheets():
       print('Name: {} Rows: {} Cols: {}'.format(sh.name, sh.ncols, sh.nrows))
       
   def convert(self):
+    with open(dest_file,'w') as fh_out:
+      dw = csv.DictWriter(fh_out, fieldnames=['A','B'], lineterminator='\n')
+      dw.writeheader()
+      dw.writerow({'A': 1, 'B': 'Not yet implemented'})
+      
     print('Not yet implemented')
     
